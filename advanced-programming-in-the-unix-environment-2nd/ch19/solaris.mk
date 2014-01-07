4b3f7785d7fa2f5ed0f6ef9fd2693ef06a83c01c
Jianlong Chen <jianlong99@gmail.com>
2014-01-07 23:45:53 +0800

    Fix last commit error

--- advanced-programming-in-the-unix-environment-2nd/ch19/solaris.mk
+++ advanced-programming-in-the-unix-environment-2nd/ch19/solaris.mk
@@ -1,14 +1,18 @@
 include ../Make.defines.solaris
 
-PROGS =	fig18.10\
-		fig18.11\
-		fig18.16\
-		fig18.18\
-		fig18.21\
-		fig18.22\
-		ex18.1
+PROGS =	pty\
+		pty2\
+		ex19.10\
+		ex19.10b
 
 all:	${PROGS}
+
+pty: fig19.12.o fig19.13.o fig19.17.o
+	$(LINK.c) -g -o $@ $^ $(LIB)
+
+# use select version loop
+pty2: fig19.12.o ex19.4.o fig19.17.o
+	$(LINK.c) -g -o $@ $^ $(LIB)
 
 savedid: savedid.o
 		$(LINK.c) -o savedid savedid.o $(LDLIBS)
