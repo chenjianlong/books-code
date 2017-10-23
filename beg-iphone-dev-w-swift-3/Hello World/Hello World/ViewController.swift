99def4e5b3a6bffc623b4cd1f59e6b25b7b04735
Jianlong Chen <jianlong99@gmail.com>
2017-10-24 07:12:01 +0800

    Add iOS hello world app

---
+++ beg-iphone-dev-w-swift-3/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by chenjianlong on 2017/10/24.
+//  Copyright © 2017年 MyCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
