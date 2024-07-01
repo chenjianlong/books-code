b0777179b05d6e244489f47bd8ee26b948bb318d
Jianlong Chen <jianlong99@gmail.com>
2024-07-01 20:37:52 +0800

    Change tyrchen-geektime-rust scrape_url2 return type

--- tyrchen-geektime-rust/03_grammar/scrape_url2/src/main.rs
+++ tyrchen-geektime-rust/03_grammar/scrape_url2/src/main.rs
@@ -1,16 +1,18 @@
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let url = &args[1];
     let output = "rust.md";
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to markdown...") ;
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("Converted markdown has been saved in {}.", output);
+
+    Ok(())
 }
